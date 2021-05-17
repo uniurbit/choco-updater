ca0b592035932fa7c42bdd0940598e2a11287376
franz-dev <72202910+franz-dev@users.noreply.github.com>
2021-05-17 10:08:08 +0200

    Update dbstruct.py

--- dbstruct.py
+++ dbstruct.py
@@ -1,3 +1,4 @@
+# Francesco Buresta @ Uniurb 20201210
 # migrations 
 create_db = '''
 CREATE DATABASE  IF NOT EXISTS `choco_update` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
